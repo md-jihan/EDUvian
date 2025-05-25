8654dec7562dc9dc7a5b44f9dadfe15409851c24
md-jihan <monabborhossen@gmail.com>
2025-05-25 10:11:14 +0600

    first commit

--- lib/model/router.dart
+++ lib/model/router.dart
@@ -1,16 +1,16 @@
-import 'package:eduvian/screen/homeScreen.dart';
+//import 'package:eduvian/screen/homeScreen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../screen/credit.dart';
+import '../screen/credit_copy.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: '/',
     routes: [
-      GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+      //GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
       GoRoute(
-        path: '/credit',
+        path: '/',
         builder: (context, state) => const CreditCalculation(),
       ),
     ],
