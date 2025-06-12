36be8d0f9d0b741604cdcd1ca9494d78c72c0730
md-jihan <monabborhossen@gmail.com>
2025-06-12 20:51:19 +0600

    Initial commit of EDUvian Flutter project

--- lib/screen/cgpa.dart
+++ lib/screen/cgpa.dart
@@ -1,3 +1,4 @@
+import 'package:eduvian/model/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CgpaCalculation extends StatelessWidget {
@@ -5,6 +6,6 @@ class CgpaCalculation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: appBar(context, "CGPA"));
   }
 }
