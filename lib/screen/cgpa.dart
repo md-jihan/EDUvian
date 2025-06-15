b5ed96ef4b6e23b544e6ae8ab12b106ead0470de
md-jihan <monabborhossen@gmail.com>
2025-06-16 00:42:09 +0600

    Initial commit of EDUvian Flutter project

--- lib/screen/cgpa.dart
+++ lib/screen/cgpa.dart
@@ -1,3 +1,4 @@
+import 'package:eduvian/model/department.dart';
 import 'package:eduvian/model/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,6 +13,35 @@ class CgpaCalculation extends ConsumerStatefulWidget {
 class _CgpaCalculationState extends ConsumerState<CgpaCalculation> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: appBar(context, "CGPA"));
+    return Scaffold(
+      appBar: appBar(context, "CGPA"),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Consumer(
+                builder: (context, ref, child) {
+                  return RoundedField(
+                    child: DropdownButtonFormField(
+                      value: ref.watch(departmentProvider),
+                      decoration: const InputDecoration(
+                        labelText: "select Department (Optional)",
+                        border: OutlineInputBorder(borderSide: BorderSide.none),
+                      ),
+                      items: department.keys.map((dept) {
+                        return DropdownMenuItem(value: dept, child: Text(dept));
+                      }).toList(),
+                      onChanged: (value) =>
+                          ref.read(departmentProvider.notifier).state = value,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
