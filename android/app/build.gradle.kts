e2f50358263f494276060c078fae388b2eeac966
md-jihan <monabborhossen@gmail.com>
2025-06-15 23:52:19 +0600

    Initial commit of EDUvian Flutter project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "25.2.9519653"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
