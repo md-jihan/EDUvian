185d75cb46e9e0e63556a8a1f645e3238245f87b
md-jihan <monabborhossen@gmail.com>
2025-06-21 12:05:49 +0600

    Initial commit of EDUvian Flutter project

--- lib/screen/cgpa.dart
+++ lib/screen/cgpa.dart
@@ -4,7 +4,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final totalCreditProvider = StateProvider<String>((ref) => '');
 final totalGpaProvider = StateProvider<String>((ref) => '');
-final cgpaListProvider = StateProvider<List<Map<String, dynamic>>>((ref) => []);
+final SemesterListProvider = StateProvider<List<Map<String, dynamic>>>(
+  (ref) => [],
+);
+final cgpaProvider = Provider<double>((ref) {
+  final semesters = ref.watch(SemesterListProvider);
+  double totalCredit = 0;
+  double totalWetghtedGPA = 0;
+
+  for (var semester in semesters) {
+    final credit = semester['credit'] as double;
+    final gpa = semester['gpa'] as double;
+
+    totalCredit += credit;
+    totalWetghtedGPA += gpa * credit;
+  }
+  if (totalCredit == 0) return 0;
+  return totalWetghtedGPA / totalCredit;
+});
 
 class CgpaCalculation extends ConsumerStatefulWidget {
   const CgpaCalculation({super.key});
@@ -18,7 +35,7 @@ class _CgpaCalculationState extends ConsumerState<CgpaCalculation> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(context, "CGPA"),
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.all(16),
           color: offWhite,
